9893667f10f3379546c58f71fbdfab58d8e1b83c
Artash Avetisyan <143110528+artashavetisyan@users.noreply.github.com>
2023-09-19 11:03:33 +0400

    Homework4

---
+++ src/dynamicarray/DynamicArrayDemo.java
@@ -0,0 +1,14 @@
+package dynamicarray;
+
+public class DynamicArrayDemo {
+    public static void main(String[] args) {
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.add(1);
+        dynamicArray.add(55);
+        dynamicArray.add(55);
+        dynamicArray.add(55);
+        dynamicArray.getByIndex(1);
+        dynamicArray.print();
+
+    }
+}
